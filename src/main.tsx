e4796c40eede1ac662ec076b1394ffdb2d25447c
Daniel <virusxd521@gmail.com>
2023-09-02 19:07:47 +0300

    Removed comment

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "@/../app/globals.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "@/infrastructure/routing/routes";
 
-// TODO: Think which implementation would be better. Context or hoc (Michal from kindred used the hoc approach)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={routes} />
